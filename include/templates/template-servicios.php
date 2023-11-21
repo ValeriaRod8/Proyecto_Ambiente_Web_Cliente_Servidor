f5d246ce4bfca2e286983c012c96ba84935e3fce
StevenRB <retanasteven8@gmail.com>
2023-11-20 18:15:59 -0600

    Update template-servicios

--- include/templates/template-servicios.php
+++ include/templates/template-servicios.php
@@ -1,26 +1,26 @@
 <div class="servicios-content-container">
-    <div class="servicio-1">
-        <a href="servicios_pediatria.php">
+    <a href="servicios_pediatria.php">
+        <div class="servicio-1">
             <i class="fa-sharp fa-solid fa-hospital-user"></i>
             <h3>Pediatría</h3>
-        </a>
-    </div>
-    <div class="servicio-2">
-        <a href="servicios_dermatologia.php">
+        </div>
+    </a>
+    <a href="servicios_dermatologia.php">
+        <div class="servicio-2">
             <i class="fa-solid fa-bed pulse"></i>
             <h3>Dermatología</h3>
-        </a>
-    </div>
-    <div class="servicio-3">
-        <a href="servicios_cardiologia.php">
+        </div>
+    </a>
+    <a href="servicios_cardiologia.php">
+        <div class="servicio-3">
             <i class="fa-sharp fa-solid fa-hospital"></i>
             <h3>Cardiología</h3>
-        </a>
-    </div>
-    <div class="servicio-4">
-        <a href="servicios_ginecologia.php">
+        </div>
+    </a>
+    <a href="servicios_ginecologia.php">
+        <div class="servicio-4">
             <i class="fa-sharp fa-solid fa-stethoscope"></i>
             <h3>Ginecología</h3>
-        </a>
-    </div>
+        </div>
+    </a>
 </div>
