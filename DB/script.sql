e583eb5924a65f54008353611fc0fde93ddf6055
Valeria <valeriarodri169@gmail.com>
2023-12-10 02:31:18 -0600

    Update script.sql

--- DB/script.sql
+++ DB/script.sql
@@ -26,3 +26,13 @@ CREATE TABLE Reservas (
     FechaCreacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FechaModificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+CREATE TABLE cita (
+	 Id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	 Especialista varchar(20) NOT NULL,
+	 CorreoEspecialista varchar(50) NOT NULL,
+	 CorreoCliente varchar(50) NOT NULL,
+	 Especialidad varchar(20) NOT NULL,
+	 Descripcion text NOT NULL,
+	 Fecha datetime,
+	 Notas text
+ ) ENGINE=InnoDB DEFAULT CHARSET=ucitatf8mb4 COLLATE=utf8mb4_general_ci;
