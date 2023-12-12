75bcb2bad60144298406a86a78e8c9d440ce65df
JoseUrenaO <jauo2810@gmail.com>
2023-12-12 12:39:34 -0600

    Inserts productos

--- server/compra.php
+++ server/compra.php
@@ -6,11 +6,11 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-if (isset($_SESSION['login'])) {
-    $id = $_SESSION['codigo'];
+if (isset($_SESSION['username'])) {
+    $codigo = $_SESSION['codigo'];
     $nombre = $_SESSION['nombre'];
     $carrito = json_decode($_POST['carrito'], true);
-    if (insertarFactura($id, $username, $carrito)) {
+    if (insertarFactura($codigo, $nombre, $carrito)) {
         echo json_encode(array('success' => true, 'message' => 'Inserción exitosa', 'carrito' => $carrito));
     } else {
         echo json_encode(array('success' => false, 'message' => 'Error en la inserción'));
